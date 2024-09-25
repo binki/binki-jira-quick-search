0f35f3afcfae451bdc4828a7916de91c1784b89d
Nathan Phillip Brink <ohnobinki@ohnopublishing.net>
2024-09-25 15:20:18 +0000

    Fix initial redirect to the setup page.

--- src/App.tsx
+++ src/App.tsx
@@ -16,36 +16,45 @@ function App() {
     };
   }, []);
 
-  const [, uriBase, hashPath, hashParams] = (/([^#]*)#([^?]*)\??(.*)/.exec(href) || []);
-  const setHash = React.useCallback(() => {
-    return function (path:string, params?:{[key:string]:string}) {
+  const [, uriBase, hashPath, hashParams] = (/([^#]*)(?:#([^?]*)\??(.*))?/.exec(href) || []);
+  const setHash = React.useCallback((path:string, params?:{[key:string]:string}) => {
       const serializedParams = params ? Object.keys(params).map(key => `${encodeURIComponent(key)}=${encodeURIComponent(String(params[key]))}`).join('&') : '';
       window.location.href = uriBase + '#' + path + (serializedParams.length ? '?' + serializedParams : '');
-    };
   }, [uriBase]);
-  if (hashPath) {
-    const params = Object.create(null);
-    for (const pair of (hashParams || '').split(/&/g).map(param => param.split('='))) {
-      if (pair[0]) {
-        params[decodeURIComponent(pair[0])] = decodeURIComponent(pair.slice(1).join('='));
+  const goodRender = (() => {
+    if (hashPath) {
+      const params = Object.create(null);
+      for (const pair of (hashParams || '').split(/&/g).map(param => param.split('='))) {
+        if (pair[0]) {
+          params[decodeURIComponent(pair[0])] = decodeURIComponent(pair.slice(1).join('='));
+        }
+      }
+      if (hashPath === 'setup') {
+        return (
+          <Setup setHash={setHash}/>
+        );
+      }
+      if (hashPath === 'view') {
+        return (
+          <View params={params} setHash={setHash}/>
+        );
       }
     }
-    if (hashPath === 'setup') {
-      return (
-        <Setup setHash={setHash}/>
-      );
+  })();
+  const isGoodRender = !!goodRender;
+  React.useEffect(() => {
+    if (!isGoodRender) {
+      // Just redirect them to something valid.
+      setHash('setup');
     }
-    if (hashPath === 'view') {
-      return (
-        <View params={params} setHash={setHash}/>
-      );
-    }
-  }
-  // Just redirect them to something valid.
-  window.location.href = uriBase + '#setup';
+  }, [isGoodRender, setHash]);
+  if (goodRender) {
+    return goodRender;
+  } else {
   return (
-    <div/>
+    <div>Redirecting…</div>
   );
+    }
 }
 
 export default App;
